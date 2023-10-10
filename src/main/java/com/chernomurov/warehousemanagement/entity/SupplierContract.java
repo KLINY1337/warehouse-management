7e71aa05e88bfc91a903387daf63279ca746b68b
KLINY1337 <semyon.chernomurov@gmail.com>
2023-10-10 20:31:02 +0300

    Modified entities: SupplierContract.java
    Description: modified entity for linking shipments with suppliers

--- src/main/java/com/chernomurov/warehousemanagement/entity/SupplierContract.java
+++ src/main/java/com/chernomurov/warehousemanagement/entity/SupplierContract.java
@@ -1,15 +1,15 @@
 package com.chernomurov.warehousemanagement.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -32,4 +32,20 @@ public class SupplierContract {
     private LocalDateTime storageTime;
 
     private String storingConditions;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        SupplierContract that = (SupplierContract) o;
+        return getShipment() != null && Objects.equals(getShipment(), that.getShipment());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
